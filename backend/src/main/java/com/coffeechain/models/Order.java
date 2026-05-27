eef334ac0b78af1645db20a6d3576af7ee6b9fb3
Bia <popescubiancapatricia@gmail.com>
2026-05-27 15:50:47 +0300

    order class

--- backend/src/main/java/com/coffeechain/models/Order.java
+++ backend/src/main/java/com/coffeechain/models/Order.java
@@ -1,4 +1,17 @@
 package com.coffeechain.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
+    private String customerUsername;
+    private Map<String, Integer> orderedItems;
+    private String status;
+    private String estimatedTime;
+    private String rejectionReason;
 }
