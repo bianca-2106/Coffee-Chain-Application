6d81426189c53f6dd3a0b80f43af2bf37a9efc1b
Bia <popescubiancapatricia@gmail.com>
2026-05-27 15:45:52 +0300

    user class

--- backend/src/main/java/com/coffeechain/models/User.java
+++ backend/src/main/java/com/coffeechain/models/User.java
@@ -1,4 +1,18 @@
 package com.coffeechain.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private String username;
+    private String password;
+    private String role;
+
+    private String fullName;
+    private String address;
+    private String phoneNumber;
 }
