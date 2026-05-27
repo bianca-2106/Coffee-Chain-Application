0ccefff6a63cde1cf3322504efc76364d63aa84e
Bia <popescubiancapatricia@gmail.com>
2026-05-27 15:56:35 +0300

    menuitem class

--- backend/src/main/java/com/coffeechain/models/MenuItem.java
+++ backend/src/main/java/com/coffeechain/models/MenuItem.java
@@ -1,4 +1,15 @@
 package com.coffeechain.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MenuItem {
+    private String name;
+    private double price;
+    private int availableQuantity;
+    private String picturePath;
 }
